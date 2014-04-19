79280976ba29a08304129daa2c853250336607f4
Пронин Андрей <kanekt@bk.ru>
2014-04-19 21:03:47 +0400

    Update Width Table
    In Russian Options width must be 15%

--- admin/pages.php
+++ admin/pages.php
@@ -102,7 +102,7 @@ if ($action=='view'){
             ),
           // array of options, in this case entries for dropdown
           array(
-            'widths'=>'50|20|20',
+            'widths'=>'50|20|15',
             "status"=>array('Published'=>__("PUBLISHED"),'Draft'=>__("DRAFT"))
             ), true
         );
@@ -110,7 +110,7 @@ if ($action=='view'){
       $children = $table->reload();
       foreach ($records  as $record) {
        $table->htmlTableRow($record,array(
-            'widths'=>'5|50|20|15',
+            'widths'=>'5|45|20|15',
             "status"=>array('Published'=>__("PUBLISHED"),'Draft'=>__("DRAFT"))
             ), true); 
 
